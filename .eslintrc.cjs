3093c692f9b3b2981c15726ed70ce0bf50abf410
base-stone <leafront@126.com>
2023-12-13 14:00:20 +0800

    update src/.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,7 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    'prettier/prettier': 'error',
     '@typescript-eslint/no-this-alias': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off'
