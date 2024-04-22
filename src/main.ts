cf641a95377d613436506832830e80ded6c4059d
base-stone <leafront@126.com>
2024-04-22 17:05:29 +0800

    delete request.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,2 @@
-import httpRequest, { cancelRequest } from './request'
-import type { RequestFn } from './request'
 export * from './utils'
 export * from './store'
-
-export { RequestFn, httpRequest, cancelRequest }
